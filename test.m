44bca2457aab3b5b2253940e7136420bf8492ec7
zhishiwanwan01 <609429923@qq.com>
2015-07-20 22:20:06 +1000

    functions

--- test.m
+++ test.m
@@ -1,1 +1,5 @@
-%% test
+numberOfCharacters = 100;
+frameLength = 10;
+[ source ] = randomSource( numberOfCharacters, frameLength );
+symbolFrame = source(1, :);
+[ bitSequence ] = c12_Encoder( symbolFrame );
