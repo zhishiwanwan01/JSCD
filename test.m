96bff22394220f5b4a029caead7f34a81929fb4b
zhishiwanwan01 <609429923@qq.com>
2015-07-20 22:24:01 +1000

    small change

--- test.m
+++ test.m
@@ -2,4 +2,4 @@ numberOfCharacters = 100;
 frameLength = 10;
 [ source ] = randomSource( numberOfCharacters, frameLength );
 symbolFrame = source(1, :);
-[ bitSequence ] = c12_Encoder( symbolFrame );
+[ bitSequence ] = C12_Encoder( symbolFrame );
